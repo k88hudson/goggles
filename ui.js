bcb35dcec38ddb17a872dcc76fea1fed2813c918
Kate Hudson <katehudsondesign@gmail.com>
2013-01-24 11:38:15 -0500

    tab stuff

--- ui.js
+++ ui.js
@@ -3,24 +3,42 @@ var currenturlel = document.querySelector( ".xg-current-url" );
 var testdiv = document.querySelector( ".test-div" );
 
 var xgcontainer = document.querySelector( ".xg-container" );
-var input = document.querySelector( "textarea" );
+var textinput = document.querySelector( "[data-tab=text]" );
+var hrefinput = document.querySelector( "[data-tab=href]" );
+
+var treetabs = document.querySelector( ".xg-tree" );
+var tabcontainer = document.querySelector( ".tab-container" );
 
 currenturlel.innerHTML = window.location.href;
 
 testdiv.addEventListener( "click", function( e ) {
   e.preventDefault();
   xgcontainer.classList.toggle( "collapsed" );
-  input.value = this.innerHTML;
+  testdiv.classList.toggle( "xg-editing" );
+  textinput.value = this.innerHTML;
+  hrefinput.value = this.href;
 }, false );
 
-input.addEventListener( "keyup", function(e) {
+textinput.addEventListener( "keyup", function(e) {
   testdiv.innerHTML = this.value;
 }, false );
 
-input.addEventListener( "focus", function(e) {
-  testdiv.classList.add( "xg-editing" );
+textinput.addEventListener( "focus", function(e) {
+  testdiv.classList.add( "xg-focus" );
 }, false );
 
-input.addEventListener( "blur", function(e) {
-  testdiv.classList.remove( "xg-editing" );
+textinput.addEventListener( "blur", function(e) {
+  testdiv.classList.remove( "xg-focus" );
+}, false );
+
+treetabs.addEventListener( "click", function(e) {
+  var value = e.target.getAttribute( "data-value" );
+  var currentselected = treetabs.querySelector( "[data-selected]" );
+  var currentselectedtab = tabcontainer.querySelector( "[data-selected]" );
+  if ( value ) {
+    currentselected.removeAttribute( "data-selected" );
+    currentselectedtab.removeAttribute( "data-selected" );
+    e.target.setAttribute( "data-selected", true );
+    tabcontainer.querySelector( "[data-tab=" + value + "]" ).setAttribute( "data-selected", true );
+  }
 }, false );
